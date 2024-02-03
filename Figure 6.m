5f13698f2244183db6cf1cfc97afcd06fba08dae
Jerry Lin <jiaren.lin@gmail.com>
2024-02-03 00:04:52 -0500

    Update Figure 6.m

--- Figure 6.m
+++ Figure 6.m
@@ -15,3 +15,31 @@ title('Ki67+ cells');
 ylabel('Percent positive');
 xlabel('Recurrence');
 set(gca,'xticklabels',{'No','Yes'});
+
+%% Figure 6B
+
+
+test1 = sumGroup1Cyt{ismember(sumGroup1Cyt.Code,'Her2-5'),88:94};
+test2 = sumGroup1Cyt{ismember(sumGroup1Cyt.Code,'Her2-161'),88:94};
+test3 = sumGroup1Cyt{ismember(sumGroup1Cyt.Code,'Her2-164'),88:94};
+test4 = sumGroup1Cyt{ismember(sumGroup1Cyt.Code,'Her2-70'),88:94};
+
+figure,bar([test1;test2;test3;test4]'*100);
+ytickformat('percentage');
+xlabel('Cluster');
+ylabel('Percentage of cells');
+legend('HER2-5','HER2-161','HER2-164','HER2-70');
+
+%% Figure 6F
+figure,myboxplot2(sumGroup1Cyt.EM7distNew,sumGroup1Cyt.RecurrenceN);
+title('GMMscore');
+xlabel('Recurrence');
+set(gca,'xticklabels',{'No','Yes'});
+
+%% Figure 6G
+
+figure,gscatter(sumGroup1Cyt.EM7distNew,sumGroup1Cyt.mean_ddSNE,sumGroup1Cyt.RecurrenceN);
+xlabel('GMM score');
+ylabel('tSNE score');
+title('Recurrence vs. Heterogeneity');
+legend('No recurrence','recurrence');
